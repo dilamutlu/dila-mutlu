12a72f95bfab80fc6665d57a7751fcb71b72436e
dilamutlu <dilamutlu81@gmail.com>
2024-11-03 22:23:03 +0300

    ilk 100 asal sayı yazdırıldı

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 print("Merhaba Dünya!")
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+primes = []
+count = 0
+num = 2
+
+while count < 100:
+    if is_prime(num):
+        primes.append(num)
+        count += 1
+    num += 1
+
+print("İlk 100 Asal Sayı Listesi =>")
+print(", ".join(map(str, primes)))
